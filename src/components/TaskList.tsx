284a5b49a4bc07ad8348de2762761c969c00da57
Igor Patrick Ponticelli <patrickigor.ip@gmail.com>
2021-10-19 22:25:12 -0300

    Adicionado as funcionalidades
    Funcionalidade de aparecer a tarefa na tela, de marcar como concluída e de remover

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,30 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const newTasks = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(tasks => [...tasks, newTasks])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasksComplete = tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task);
+    setTasks(newTasksComplete)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+    setTasks(removeTask);
   }
 
   return (
